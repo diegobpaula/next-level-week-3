60cf3661d39af0259b3d3c8f3f20a3ce97357fbe
Diego de Paula <dih.ferratone@gmail.com>
2020-10-14 23:53:30 -0400

    insert page-orphanages and map

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,28 @@
+// create map
+const map = L.map("mapid").setView([-3.1192176,-60.0227363], 14);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1 classe="choose-orphanages"> <img src="./public/images/arrow-white.svg" ></a>')
+
+
+// create and add marker
+L.marker([-3.1192176,-60.0227363], {icon})
+  .addTo(map)
+  .bindPopup(popup)
+  .openPopup();
